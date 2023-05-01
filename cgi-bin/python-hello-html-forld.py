1385b4ef684c85cc6c21858b9288f294b648bf39
arifd <ademirkan@ucsd.edu>
2023-05-01 12:39:28 -0700

    testing python hello world html

---
+++ cgi-bin/python-hello-html-forld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+print("Content-Type: text/html\n")
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
+print("<h1>Hello, Python!</h1>")
+print("</body>")
+print("</html>")
