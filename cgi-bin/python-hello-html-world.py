254aea8d95ca398da93dc16f98a14893dc0fbc74
arifd <ademirkan@ucsd.edu>
2023-05-01 12:45:39 -0700

    testing python html world

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -1,11 +1,26 @@
+
 #!/usr/bin/env python3
 
-print("Content-Type: text/html\n")
+import os
+from datetime import datetime
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n")
+
 print("<html>")
 print("<head>")
 print("<title>Hello, Python!</title>")
 print("</head>")
 print("<body>")
-print("<h1>Hello, Python!</h1>")
+
+print("<h1>Thomas was here - Hello, Python!</h1>")
+print("<p>This page was generated with the Python programming language</p>")
+
+date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"<p>Current Time: {date}</p>")
+
+address = os.environ.get("REMOTE_ADDR", "Unknown")
+print(f"<p>Your IP Address: {address}</p>")
+
 print("</body>")
 print("</html>")
